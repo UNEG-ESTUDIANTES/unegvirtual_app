bee0a6ae30591a950e06395c0d2ba8792b5e1682
Alvaro J. Resplandor <alvarojrr79@gmail.com>
2023-06-22 23:18:49 -0400

    Make user info responsive

--- lib/features/user/presentation/widgets/basic_info_widget.dart
+++ lib/features/user/presentation/widgets/basic_info_widget.dart
@@ -9,18 +9,55 @@ class BasicInfoWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth > 600) {
+          return Row(
+            children: const [
+              CircleAvatar(
+                backgroundImage: AssetImage(
+                  'assets/profile_xample.png',
+                ),
+                radius: 80,
+              ),
+              SizedBox(width: 24),
+              _UserDescription(),
+            ],
+          );
+        }
+
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: const [
+            CircleAvatar(
+              backgroundImage: AssetImage(
+                'assets/profile_xample.png',
+              ),
+              radius: 48,
+            ),
+            SizedBox(height: 16),
+            _UserDescription(
+              crossAxisAlignment: CrossAxisAlignment.center,
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+class _UserDescription extends StatelessWidget {
+  final CrossAxisAlignment? crossAxisAlignment;
+
+  const _UserDescription({this.crossAxisAlignment});
+
+  @override
+  Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
 
     return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
+      crossAxisAlignment: crossAxisAlignment ?? CrossAxisAlignment.start,
       children: [
-        const CircleAvatar(
-          backgroundImage: AssetImage(
-            'assets/profile_xample.png',
-          ),
-          radius: 48,
-        ),
-        const SizedBox(height: 16),
         Text(
           'Xiao Yaksha',
           style: textTheme.titleLarge?.copyWith(
