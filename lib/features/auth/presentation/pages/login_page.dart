6476a9bf0104ef198548480018dd1f78cf20517f
Alvaro J. Resplandor <alvarojrr79@gmail.com>
2023-06-27 09:14:53 -0400

    Replace container and add padding

--- lib/features/auth/presentation/pages/login_page.dart
+++ lib/features/auth/presentation/pages/login_page.dart
@@ -9,119 +9,115 @@ class LogInPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey.shade100,
       body: SingleChildScrollView(
-        padding: const EdgeInsets.only(top: 120),
+        padding: const EdgeInsets.all(24.0),
         child: Center(
-          child: Container(
-            height: 550,
-            width: 300,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              color: const Color.fromARGB(255, 255, 255, 255),
-            ),
-            child: Column(
-              children: [
-                const SizedBox(height: 20),
-                const Image(
-                  image: AssetImage(
-                    'assets/uneg_logo_color.png',
-                  ),
-                  width: 100,
-                  height: 100,
-                  fit: BoxFit.contain,
+          child: SizedBox(
+            width: 400,
+            child: Card(
+              surfaceTintColor: Colors.white,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 24.0,
+                  vertical: 32.0,
                 ),
-                const SizedBox(height: 10),
-                const Text(
-                  "Accede al aula virtual",
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(height: 10),
-                const Text(
-                  "Si eres estudiante regular de la UNEG, ingresa al sistema con Cedula de Identidad",
-                  style: TextStyle(fontSize: 18),
-                  textAlign: TextAlign.center,
-                ),
-                const SizedBox(height: 10),
-                SizedBox(
-                  width: 250,
-                  height: 55,
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: "Correo Electronico o Usuario",
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(18),
+                child: Column(
+                  children: [
+                    const Image(
+                      image: AssetImage(
+                        'assets/uneg_logo_color.png',
+                      ),
+                      width: 80,
+                      height: 80,
+                      fit: BoxFit.contain,
+                    ),
+                    const SizedBox(height: 16),
+                    const Text(
+                      "Accede al aula virtual",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
-                  ),
-                ),
-                const SizedBox(height: 10),
-                SizedBox(
-                  width: 250,
-                  height: 55,
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: "Contraseña",
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(18),
+                    const SizedBox(height: 8),
+                    const Text(
+                      "Si eres estudiante regular de la UNEG, ingresa al sistema con Cedula de Identidad",
+                      style: TextStyle(fontSize: 18),
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 24),
+                    TextField(
+                      decoration: InputDecoration(
+                        hintText: "Correo Electrónico o Usuario",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(18),
+                        ),
                       ),
                     ),
-                  ),
-                ),
-                const SizedBox(height: 10),
-                TextButton(
-                  onPressed: (() {}),
-                  style: TextButton.styleFrom(
-                    backgroundColor: Colors.blue,
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(18),
+                    const SizedBox(height: 16),
+                    TextField(
+                      decoration: InputDecoration(
+                        hintText: "Contraseña",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(18),
+                        ),
                       ),
                     ),
-                    fixedSize: const Size(250, 30),
-                  ),
-                  child: const Text(
-                    "Iniciar Seción",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 10),
-                const Text(
-                  "¿Olvido su nombre de usuario o contraseña?",
-                  style: TextStyle(
-                    fontSize: 14,
-                    color: Colors.blue,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-                const SizedBox(height: 10),
-                TextButton(
-                  onPressed: (() {}),
-                  style: TextButton.styleFrom(
-                    backgroundColor: Colors.grey,
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(18),
+                    const SizedBox(height: 24),
+                    TextButton(
+                      onPressed: (() {}),
+                      style: TextButton.styleFrom(
+                        backgroundColor: Colors.blue,
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(18),
+                          ),
+                        ),
+                        fixedSize: const Size(250, 30),
+                      ),
+                      child: const Text(
+                        "Iniciar Seción",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
-                    fixedSize: const Size(250, 30),
-                  ),
-                  child: const Text(
-                    "Iniciar Seción como invitado",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold,
+                    const SizedBox(height: 16),
+                    const Text(
+                      "¿Olvido su nombre de usuario o contraseña?",
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.blue,
+                      ),
+                      textAlign: TextAlign.center,
                     ),
-                  ),
-                )
-              ],
+                    const SizedBox(height: 16),
+                    TextButton(
+                      onPressed: (() {}),
+                      style: TextButton.styleFrom(
+                        backgroundColor: Colors.grey,
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(18),
+                          ),
+                        ),
+                        fixedSize: const Size(250, 30),
+                      ),
+                      child: const Text(
+                        "Iniciar Seción como invitado",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
             ),
           ),
         ),
