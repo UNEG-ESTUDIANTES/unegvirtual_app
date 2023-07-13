51b96f0d9e15ea169da8f64357c5beab7a307342
Alvaro J. Resplandor <alvarojrr79@gmail.com>
2023-07-12 23:45:53 -0400

    Create the db provider

---
+++ lib/core/databases/db_provider.dart
@@ -0,0 +1,67 @@
+import 'dart:async';
+
+import 'package:sqflite/sqflite.dart';
+
+import 'package:classroom_app/core/models/access_token_model.dart';
+
+class DBProvider {
+  /// The [path] to store this database.
+  final String path;
+  Database? _database;
+
+  DBProvider({required this.path});
+
+  /// The database instance.
+  Future<Database> get database async {
+    // Returns database in case is already instanced
+    if (_database != null) return _database!;
+
+    // Initialize DB
+    _database = await initDB();
+    return _database!;
+  }
+
+  /// Creates the tables in the [db].
+  Future<void> _onCreate(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE Token(
+        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+        token TEXT UNIQUE NOT NULL
+      )
+    ''');
+  }
+
+  /// Initializes the database.
+  Future<Database> initDB() async {
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _onCreate,
+    );
+  }
+
+  /// Insertes a new [accessToken].
+  Future<int> addToken(AccessTokenModel accessToken) async {
+    final db = await database;
+
+    return await db.insert(
+      'Token',
+      accessToken.toJson(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  /// Removes the latest stored token.
+  Future<int> removeToken() async {
+    final db = await database;
+
+    /// Gets the latest token.
+    final latestToken = await db.query('Token', limit: 1, orderBy: 'id DESC');
+
+    return await db.delete(
+      'Token',
+      where: 'id = ?',
+      whereArgs: [latestToken.last['id']],
+    );
+  }
+}
---
+++ test/core/databases/db_provider_test.dart
@@ -0,0 +1,70 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+
+import 'package:classroom_app/core/databases/db_provider.dart';
+import 'package:classroom_app/core/models/access_token_model.dart';
+
+/// Initialize sqflite for test.
+void sqfliteTestInit() {
+  // Initialize ffi implementation.
+  sqfliteFfiInit();
+
+  // Set global factory.
+  databaseFactory = databaseFactoryFfi;
+}
+
+Future main() async {
+  sqfliteTestInit();
+
+  late DBProvider dbProvider;
+
+  setUp(() async {
+    dbProvider = DBProvider(path: inMemoryDatabasePath);
+  });
+
+  const tAccessTokenModel = AccessTokenModel('test');
+
+  test(
+    'should create the Token table',
+    () async {
+      // arrange
+      final db = await dbProvider.database;
+
+      // act
+      final tables = await db.query(
+        'sqlite_master',
+        where: 'name = ?',
+        whereArgs: ['Token'],
+      );
+
+      // assert
+      expect(tables.length, equals(1));
+    },
+  );
+
+  group('addToken', () {
+    test(
+      'should insert the token',
+      () async {
+        // act
+        final result = await dbProvider.addToken(tAccessTokenModel);
+
+        // assert
+        expect(result, 1);
+      },
+    );
+  });
+
+  group('removeToken', () {
+    test(
+      'should remove the latest token',
+      () async {
+        // act
+        final result = await dbProvider.removeToken();
+
+        // assert
+        expect(result, 1);
+      },
+    );
+  });
+}
