46b61c2d83fc1e87260de0177524647c5052928d
Alvaro J. Resplandor <alvarojrr79@gmail.com>
2023-07-13 22:10:56 -0400

    Implement auth remote data source

--- lib/features/auth/data/data_sources/auth_remote_data_source.dart
+++ lib/features/auth/data/data_sources/auth_remote_data_source.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
 import 'package:classroom_app/core/error/exceptions.dart';
 import 'package:classroom_app/core/models/access_token_model.dart';
 import 'package:classroom_app/features/auth/data/models/user_credentials_model.dart';
@@ -5,7 +9,39 @@ import 'package:classroom_app/features/auth/data/models/user_credentials_model.d
 abstract class AuthRemoteDataSource {
   /// Calls the /v1/login endpoint.
   ///
-  /// Throws an [UserCredentialsMismatchException] when the user is not found.
+  /// Throws an [UserCredentialsMismatchException] when the user credentials
+  /// doesn't match.
+  ///
   /// Otherwise throws a [ServerException] for all other error codes.
   Future<AccessTokenModel> login(UserCredentialsModel userCredentials);
+}
+
+class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
+  final http.Client client;
+
+  AuthRemoteDataSourceImpl({required this.client});
+
+  @override
+  Future<AccessTokenModel> login(UserCredentialsModel userCredentials) async {
+    try {
+      final response = await client.post(
+        Uri.http(
+          '0.0.0.0:3000',
+          '/v1/login',
+        ),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: userCredentials.toJson(),
+      );
+
+      if (response.statusCode == 401) throw UserCredentialsMismatchException();
+
+      if (response.statusCode != 200) throw ServerException();
+
+      return AccessTokenModel.fromJson(json.decode(response.body));
+    } on http.ClientException {
+      throw ServerException();
+    }
+  }
 }
---
+++ test/features/auth/data/data_sources/auth_remote_data_source_test.dart
@@ -0,0 +1,179 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart' as http;
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'package:classroom_app/core/error/exceptions.dart';
+import 'package:classroom_app/core/models/access_token_model.dart';
+import 'package:classroom_app/features/auth/data/data_sources/auth_remote_data_source.dart';
+import 'package:classroom_app/features/auth/data/models/user_credentials_model.dart';
+
+import '../../../../fixtures/fixture_reader.dart';
+
+@GenerateNiceMocks([MockSpec<http.Client>()])
+import 'auth_remote_data_source_test.mocks.dart';
+
+void main() {
+  late MockClient client;
+  late AuthRemoteDataSourceImpl dataSourceImpl;
+
+  setUp(() {
+    client = MockClient();
+    dataSourceImpl = AuthRemoteDataSourceImpl(client: client);
+  });
+
+  const tAccessTokenModel = AccessTokenModel('test');
+
+  const tUserCredentialsModel = UserCredentialsModel(
+    email: 'test',
+    password: 'test',
+  );
+
+  group('login', () {
+    void setUpMockHttpClientSuccess200() {
+      when(
+        client.post(
+          any,
+          headers: anyNamed('headers'),
+          body: anyNamed('body'),
+        ),
+      ).thenAnswer(
+        (_) async => http.Response(
+          fixture('access_token.json'),
+          200,
+        ),
+      );
+    }
+
+    void setUpMockHttpClientError401() {
+      when(
+        client.post(
+          any,
+          headers: anyNamed('headers'),
+          body: anyNamed('body'),
+        ),
+      ).thenAnswer(
+        (_) async => http.Response(
+          'Invalid Credentials',
+          401,
+        ),
+      );
+    }
+
+    void setUpMockHttpClientGeneralError() {
+      when(
+        client.post(
+          any,
+          headers: anyNamed('headers'),
+          body: anyNamed('body'),
+        ),
+      ).thenAnswer(
+        (_) async => http.Response(
+          'Unknown Error',
+          500,
+        ),
+      );
+    }
+
+    void setUpMockHttpClientException() {
+      when(
+        client.post(
+          any,
+          headers: anyNamed('headers'),
+          body: anyNamed('body'),
+        ),
+      ).thenThrow(http.ClientException(''));
+    }
+
+    test(
+      '''should perform a POST request with the user 
+      credentials and with the application/json header''',
+      () async {
+        // arrange
+        setUpMockHttpClientSuccess200();
+
+        // act
+        dataSourceImpl.login(tUserCredentialsModel);
+
+        // assert
+        verify(
+          client.post(
+            Uri.http(
+              '0.0.0.0:3000',
+              '/v1/login',
+            ),
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: tUserCredentialsModel.toJson(),
+          ),
+        );
+      },
+    );
+
+    test(
+      'should return AccessTokenModel when status code is 200 (success)',
+      () async {
+        // arrange
+        setUpMockHttpClientSuccess200();
+
+        // act
+        final result = await dataSourceImpl.login(tUserCredentialsModel);
+
+        // assert
+        expect(result, tAccessTokenModel);
+      },
+    );
+
+    test(
+      'should throw a UserCredentialsMismatchException when status code is 401',
+      () async {
+        // arrange
+        setUpMockHttpClientError401();
+
+        // act
+        final call = dataSourceImpl.login;
+
+        // assert
+        expect(
+          () => call(tUserCredentialsModel),
+          throwsA(const TypeMatcher<UserCredentialsMismatchException>()),
+        );
+      },
+    );
+
+    test(
+      'should throw a ServerException when status code is other than 200 and 401',
+      () async {
+        // arrange
+        setUpMockHttpClientGeneralError();
+
+        // act
+        final call = dataSourceImpl.login;
+
+        // assert
+        expect(
+          () => call(tUserCredentialsModel),
+          throwsA(const TypeMatcher<ServerException>()),
+        );
+      },
+    );
+
+    test(
+      'should throw a ServerException when the call throws a ClientException',
+      () async {
+        // arrange
+        setUpMockHttpClientException();
+
+        // act
+        final call = dataSourceImpl.login;
+
+        // assert
+        expect(
+          () => call(tUserCredentialsModel),
+          throwsA(const TypeMatcher<ServerException>()),
+        );
+      },
+    );
+  });
+}
