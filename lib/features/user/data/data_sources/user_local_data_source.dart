a30970a4e1e66760f85ccf3f2236bad536650572
Alvaro J. Resplandor <alvarojrr79@gmail.com>
2023-07-17 21:47:35 -0400

    Implement user local data source

--- lib/features/user/data/data_sources/user_local_data_source.dart
+++ lib/features/user/data/data_sources/user_local_data_source.dart
@@ -1,3 +1,5 @@
+import 'package:classroom_app/core/databases/db_provider.dart';
+import 'package:classroom_app/core/error/exceptions.dart';
 import 'package:classroom_app/core/models/user_model.dart';
 
 abstract class UserLocalDataSource {
@@ -11,4 +13,28 @@ abstract class UserLocalDataSource {
 
   /// Stores the [user] locally.
   Future<void> cacheUser(UserModel user);
+}
+
+class UserLocalDataSourceImpl implements UserLocalDataSource {
+  final DBProvider database;
+
+  UserLocalDataSourceImpl({required this.database});
+
+  @override
+  Future<void> cacheUser(UserModel user) async {
+    // Removes last user profile.
+    await database.removeUserProfile();
+
+    // Adds new user profile.
+    await database.addUserProfile(user);
+  }
+
+  @override
+  Future<UserModel> getCurrentUser() async {
+    final user = await database.getUserProfile();
+
+    if (user == null) throw NotFoundException();
+
+    return user;
+  }
 }
