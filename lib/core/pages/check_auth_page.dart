6ccd0ae8a3893257bc78b1be257d0207bafd260b
Alvaro J. Resplandor <alvarojrr79@gmail.com>
2023-07-24 16:49:43 -0400

    Fix check auth page

--- lib/core/pages/check_auth_page.dart
+++ lib/core/pages/check_auth_page.dart
@@ -5,7 +5,6 @@ import 'package:provider/provider.dart';
 import 'package:classroom_app/core/pages/main_page.dart';
 import 'package:classroom_app/core/providers/auth_provider.dart';
 import 'package:classroom_app/core/providers/page_state.dart';
-import 'package:classroom_app/core/providers/user_provider.dart';
 import 'package:classroom_app/features/landing/presentation/pages/landing_page.dart';
 
 class CheckAuthPage extends StatelessWidget {
@@ -27,13 +26,10 @@ class CheckAuthPage extends StatelessWidget {
     final authProvider = context.watch<AuthProvider>();
     final authProviderState = authProvider.state;
 
-    final userProvider = context.watch<UserProvider>();
-    final userProviderState = userProvider.state;
-
     return Scaffold(
       body: Center(
         child: FutureBuilder(
-          future: context.read<AuthProvider>().getToken(),
+          future: context.read<AuthProvider>().getAuth(),
           builder: (context, _) {
             if (authProviderState is Loading) {
               return const CircularProgressIndicator();
@@ -41,31 +37,8 @@ class CheckAuthPage extends StatelessWidget {
 
             // Try to get the user.
             if (authProviderState is Loaded) {
-              return FutureBuilder(
-                future: context.read<UserProvider>().getCurrentUser(
-                      authProvider.accessToken!,
-                    ),
-                builder: (context, _) {
-                  if (userProviderState is Loading) {
-                    return const CircularProgressIndicator();
-                  }
-
-                  // Redirect to main page.
-                  if (userProviderState is Loaded) {
-                    Future.microtask(
-                      () => _navigateTo(context, MainPage.routeName),
-                    );
-                  }
-
-                  // Redirect to landing.
-                  if (userProviderState is Error) {
-                    Future.microtask(
-                      () => _navigateTo(context, LandingPage.routeName),
-                    );
-                  }
-
-                  return Container();
-                },
+              Future.microtask(
+                () => _navigateTo(context, MainPage.routeName),
               );
             }
 
