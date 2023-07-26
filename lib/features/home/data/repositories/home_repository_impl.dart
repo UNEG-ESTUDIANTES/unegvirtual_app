70165c5f6d250864b51fcfa2bb04f0e35fb130f8
Alvaro J. Resplandor <alvarojrr79@gmail.com>
2023-07-26 09:09:50 -0400

    Use get failure in home repository

--- lib/features/home/data/repositories/home_repository_impl.dart
+++ lib/features/home/data/repositories/home_repository_impl.dart
@@ -1,10 +1,11 @@
+import 'package:dartz/dartz.dart';
+
+import 'package:classroom_app/core/utils/utils.dart';
 import 'package:classroom_app/features/home/data/datasource/home_remote_datasource.dart';
 import 'package:classroom_app/features/home/domain/repositories/home_repository.dart';
-import 'package:dartz/dartz.dart';
 
 import '../../../../core/entities/access_token.dart';
 import '../../../../core/entities/courses.dart';
-import '../../../../core/error/exceptions.dart';
 import '../../../../core/error/failures.dart';
 import '../../../../core/network/network_info.dart';
 
@@ -26,10 +27,8 @@ class HomeRepositoryImpl implements HomeRepository {
     try {
       final remoteResult = await remote.enroledCourses(accessToken);
       return Right(remoteResult);
-    } on NotEnrolledException {
-      return Left(NotEnrolledFailure());
-    } on ServerException {
-      return Left(ServerFailure());
+    } on Exception catch (exception) {
+      return Left(Utils.getFailure(exception));
     }
   }
 }
