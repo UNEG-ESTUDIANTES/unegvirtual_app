86a891418fe80a824758ab19e64b1fbb02c1556a
Alvaro J. Resplandor <alvarojrr79@gmail.com>
2023-07-31 11:19:21 -0400

    refactor: create app theme and customize fonts

---
+++ lib/core/theme/theme.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'package:google_fonts/google_fonts.dart';
+
+/// The [GoogleFonts] to be used as default.
+final _mainFont = GoogleFonts.karla();
+
+/// The [GoogleFonts] to be used on label and body.
+final _bodyFont = GoogleFonts.inter();
+
+class AppTheme {
+  static final theme = ThemeData(
+    useMaterial3: true,
+    colorSchemeSeed: Colors.blue,
+    fontFamily: _mainFont.fontFamily,
+    textTheme: TextTheme(
+      labelLarge: _bodyFont,
+      labelMedium: _bodyFont,
+      labelSmall: _bodyFont,
+      bodyLarge: _bodyFont,
+      bodyMedium: _bodyFont,
+      bodySmall: _bodyFont,
+    ),
+  );
+}
