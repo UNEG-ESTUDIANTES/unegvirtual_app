dc095e9aa3cd42e33b19374af21d0f890a7546a4
Alvaro J. Resplandor <alvarojrr79@gmail.com>
2023-08-01 10:39:36 -0400

    refactor: courses slider redesign

--- lib/features/landing/presentation/widgets/courses_slider.dart
+++ lib/features/landing/presentation/widgets/courses_slider.dart
@@ -1,40 +1,122 @@
 import 'package:flutter/material.dart';
 
-import 'package:unegvirtual_app/core/entities/courses.dart';
-import 'package:unegvirtual_app/features/landing/presentation/widgets/career_card.dart';
-import 'package:unegvirtual_app/features/landing/presentation/widgets/section.dart';
+import 'package:provider/provider.dart';
+import 'package:responsive_builder/responsive_builder.dart';
+
+import 'package:unegvirtual_app/core/entities/course.dart';
+import 'package:unegvirtual_app/core/providers/page_state.dart';
+import 'package:unegvirtual_app/features/course/presentation/providers/course_provider.dart';
 
 class CoursesSlider extends StatelessWidget {
-  /// The [careers] to display.
-  final Courses courses;
-
-  const CoursesSlider(this.courses, {super.key});
+  const CoursesSlider({super.key});
 
   @override
   Widget build(BuildContext context) {
-    const height = 250.0;
+    final textTheme = Theme.of(context).textTheme;
+    final courseProvider = context.watch<CourseProvider>();
 
-    return Section(
-      title: 'Carreras Universitarias',
-      child: courses.courses.isNotEmpty
-          ? SizedBox(
-              height: height,
-              child: ListView.separated(
-                padding: const EdgeInsets.symmetric(horizontal: 24),
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) => CourseCard(
-                  courses.courses[index],
-                  maxHeight: height,
+    // Fetch courses.
+    if (courseProvider.state is Empty) {
+      courseProvider.getCourses();
+      return Container();
+    }
+
+    // Take the courses.
+    final List<Course> courses =
+        courseProvider.courses != null ? courseProvider.courses!.courses : [];
+
+    // Select the child to display.
+    Widget child;
+
+    if (courseProvider.state is Loading) {
+      child = const Center(
+        child: CircularProgressIndicator(),
+      );
+    } else if (courses.isNotEmpty) {
+      child = ListView.separated(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          final course = courses[index];
+
+          return SizedBox(
+            width: 300,
+            child: Card(
+              margin: EdgeInsets.zero,
+              elevation: 0,
+              shape: RoundedRectangleBorder(
+                side: BorderSide(
+                  color: Theme.of(context).colorScheme.outline,
                 ),
-                separatorBuilder: (context, index) => const SizedBox(width: 16),
-                itemCount: courses.courses.length,
+                borderRadius: const BorderRadius.all(Radius.circular(12)),
               ),
-            )
-          : Container(
-              padding: const EdgeInsets.symmetric(horizontal: 24),
-              alignment: Alignment.centerLeft,
-              child: const Text('No hay carreras disponibles'),
+              child: Padding(
+                padding: const EdgeInsets.all(24.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      course.name,
+                      style: textTheme.titleMedium,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      course.description,
+                      maxLines: getValueForScreenType(
+                        context: context,
+                        mobile: 1,
+                        tablet: 2,
+                      ),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ],
+                ),
+              ),
             ),
+          );
+        },
+        separatorBuilder: (_, __) => const SizedBox(width: 16),
+        itemCount: courses.length,
+      );
+    } else {
+      child = const Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Text('No hay cursos disponibles.'),
+      );
+    }
+
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0),
+            child: Text(
+              'Cursos',
+              style: textTheme.headlineSmall,
+            ),
+          ),
+          SizedBox(
+            height: getValueForScreenType(
+              context: context,
+              mobile: 16,
+              tablet: 24,
+            ),
+          ),
+          SizedBox(
+            height: getValueForScreenType<double>(
+              context: context,
+              mobile: 100,
+              tablet: 125,
+            ),
+            child: child,
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/features/landing/presentation/widgets/landing_body.dart
+++ lib/features/landing/presentation/widgets/landing_body.dart
@@ -1,11 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'package:provider/provider.dart';
-
-import 'package:unegvirtual_app/core/entities/courses.dart';
-import 'package:unegvirtual_app/core/providers/page_state.dart';
-import 'package:unegvirtual_app/core/widgets/loading_display.dart';
-import 'package:unegvirtual_app/features/course/presentation/providers/course_provider.dart';
 import 'package:unegvirtual_app/features/landing/presentation/widgets/courses_slider.dart';
 import 'package:unegvirtual_app/features/landing/presentation/widgets/footer.dart';
 import 'package:unegvirtual_app/features/landing/presentation/widgets/hero_section.dart';
@@ -17,25 +11,12 @@ class LandingBody extends StatelessWidget {
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
 
-    final courseProvider = context.watch<CourseProvider>();
-    final state = courseProvider.state;
-
-    if (state is Empty) {
-      courseProvider.getCourses();
-      return Container();
-    }
-
-    final courses = courseProvider.courses ?? const Courses(courses: []);
-
     return SingleChildScrollView(
       child: Column(
         children: [
           HeroSection(height: height * 0.6),
           const SizedBox(height: 32),
-          if (state is Loading)
-            const LoadingDisplay()
-          else
-            CoursesSlider(courses),
+          const CoursesSlider(),
           SizedBox(height: height * 0.4),
           const Footer(),
         ],
